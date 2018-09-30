7187f7b364ec7ab66cfc22f799aae33325791251
ahmed mamdouh <ahmed_mu.mamdouh@hotmail.com>
2018-09-30 03:05:38 +0200

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.perform_deliveries = true
+
+  Depot::Application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "come.and.shop.store@gmail.com",
+    password: "onlinestore",
+    enable_starttls_auto: true
+  }
+  end
 end
