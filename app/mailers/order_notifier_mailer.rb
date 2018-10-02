92956f5d440ecae91e0cd199ec99af750cb27b29
ahmed mamdouh <ahmed_mu.mamdouh@hotmail.com>
2018-10-02 05:36:03 +0200

    add style to mailer

--- app/mailers/order_notifier_mailer.rb
+++ app/mailers/order_notifier_mailer.rb
@@ -8,12 +8,19 @@ class OrderNotifierMailer < ApplicationMailer
 
   def received(order)
     @order = order
-    mail to: order.email, subject: 'Come&Shop Order Confirmation'
+    attachments.inline["10.png"] = File.read("#{Rails.root}/app/assets/images/10.png")
+   
+    @order.line_items.each do |line_item|
+      line_item.product.image_url
+      attachments.inline["#{line_item.product.image_url}"] = File.read("#{Rails.root}/app/assets/images/#{line_item.product.image_url}")
+    end
+    
+    mail to: order.email, subject: 'Order Confirmation'
   end
 
   def shipped(order)
     @order = order
-    mail to: order.email, subject: 'Come&Shop Order Shipped'
+    mail to: order.email, subject: 'Order Shipped'
   end
 
 
